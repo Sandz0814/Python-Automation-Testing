2d9efa80feff0b3a8dd016e5eec4c891b0972d37
Sandz0814 <sandrojimena0814@gmail.com>
2023-11-20 01:00:01 +0800

    new testing

--- Alert_farmes_wondows_PageObject/nested_frames_page.py
+++ Alert_farmes_wondows_PageObject/nested_frames_page.py
@@ -1,11 +1,52 @@
+import time
+from selenium.webdriver.common.by import By
 from Tools.function import BaseDriver
 
 
 class NestedFrames(BaseDriver):
     nested_frames_btn = "//span[normalize-space()='Nested Frames']"
+    parent_iframe = "#frame1"
+    parent_iframe_text = "//body"
+    child_iframe = "//iframe[@id='frame1']"
+    child_iframe_text = "body:nth-child(2) > p:nth-child(1)"
 
     def __init__(self, driver):
         self.driver = driver
+
+    def test_nested_frame_btn(self):
+        self.find(self.nested_frames_btn).click()
+        time.sleep(2)
+
+    def test_nested_parent_iframe(self):
+
+        # Locate the parent iframe element
+        parent = self.driver.find_element(By.CSS_SELECTOR, self.parent_iframe)
+
+        # Switch to the parent iframe using the WebElement
+        self.driver.switch_to.frame(parent)
+
+        # Now you can interact with elements inside the parent iframe
+        parent_text = self.find(self.parent_iframe_text).text
+        if "Parent frame" not in parent_text:
+            assert False
+        else:
+            self.driver.save_screenshot(self.ss_url_afw + "Nested Parent Iframe.png")
+            assert True
+
+        self.driver.switch_to.default_content()
+
+    def test_nested_child_iframe(self):
+        # Locate the child iframe element
+        child = self.driver.find_element(By.XPATH, self.child_iframe)
+
+        # Switch to the child iframe using the WebElement
+        self.driver.switch_to.frame(child)
+
+        self.driver.save_screenshot(self.ss_url_afw + "Nested Child Iframe.png")
+
+        self.driver.switch_to.default_content()
+
+
 
 
 
--- testcases/test_unit.py
+++ testcases/test_unit.py
@@ -1,7 +1,6 @@
 import pytest
-from Alert_farmes_wondows_PageObject.alert_page import AlertPage
 from Alert_farmes_wondows_PageObject.brower_window_page import BrowserWindows
-from Alert_farmes_wondows_PageObject.iframe_page import FramePage
+from Alert_farmes_wondows_PageObject.nested_frames_page import NestedFrames
 from Tools.function import BaseDriver
 
 
@@ -15,10 +14,10 @@ class TestElements(BaseDriver):
         self.page_scroll()
         self.afw.test_afw_btn()
 
-        self.iframe = FramePage(self.driver)
-        self.iframe.test_frame_module_btn()
-        self.iframe.test_iframe1()
-        self.iframe.test_iframe2()
+        self.nested = NestedFrames(self.driver)
+        self.nested.test_nested_frame_btn()
+        self.nested.test_nested_parent_iframe()
+        self.nested.test_nested_child_iframe()
 
 
 
